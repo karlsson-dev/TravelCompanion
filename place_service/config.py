c07532b8694fb041a1421011fd2e5cd5f9f0d8c5
El <elvergaleev@gmail.com>
2025-04-04 11:14:50 +0300

    добавил проверку переменных окружения для подключения к бд

--- place_service/config.py
+++ place_service/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from pydantic import field_validator, ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,15 +9,41 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+
     model_config = SettingsConfigDict(
-        env_file = Path(__file__).resolve().parent.parent / ".env",
-        extra="ignore"  # Игнорировать pydantic неизвестные поля
+        env_file=Path(__file__).resolve().parent.parent / ".env",
+        extra="ignore",
     )
 
+    @field_validator("DB_PORT")
+    def validate_port(cls, port: int) -> int:
+        if not 1 <= port <= 65535:
+            raise ValueError("DB_PORT должен быть между 1 и 65535")
+        return port
 
-settings = Settings()
+    @field_validator("DB_PASSWORD")
+    def validate_password(cls, pwd: str) -> str:
+        if not pwd.strip():
+            raise ValueError("DB_PASSWORD не может быть пустым")
+        return pwd
+
+    @field_validator("DB_HOST")
+    def validate_host(cls, host: str) -> str:
+        if not host.strip():
+            raise ValueError("DB_HOST не может быть пустым")
+        if host not in ("localhost", "127.0.0.1") and "." not in host:
+            raise ValueError("DB_HOST должно быть действительным именем хоста или IP")
+        return host
+
+
+try:
+    settings = Settings()
+except ValidationError as e:
+    raise RuntimeError(f"Не верная конфигурация .env: {e}")
 
 
 def get_db():
-    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
-            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+    return (
+        f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
+        f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    )
